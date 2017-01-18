5488025fbe69f824effce0f4eb1d9c86062f5b95
samsonpaul <samsonpaul4@gmail.com>
2017-01-18 15:34:44 +0200

    brings the project to production level

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
 from slackclient import SlackClient
-from slackclient import SlackClient
 
 
 bootstrap = Bootstrap()
